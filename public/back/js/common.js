916789d487b80d2e14dda1353f5209aff448fa46
jingtian <jingtian9573@163.com>
2018-08-07 21:09:56 +0800

    二级菜单弹出，侧边栏隐藏，退出登录逻辑

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,6 +1,20 @@
 $(function () {
-
+  if (location.href.indexOf("login.html") == -1) {
+    $.ajax({
+      type: "get",
+      url: "/employee/checkRootLogin",
+      success: function (info) {
+        console.log(info);
+        if (info.success === true) {
   
+        }
+        else {
+          location.href = "login.html";
+        }
+      }
+    })
+  };
+
   $(document).ajaxStart(function () {
     NProgress.start();
   });
@@ -11,5 +25,30 @@ $(function () {
     }, 500);
   });
 
+  $(".lt_aside .category").click(function () {
+    $(".lt_aside .child").stop().slideToggle();
+  });
 
+  $(".lt_topbar .icon_menu").click(function () {
+    $(".lt_aside").toggleClass("hide_menu");
+    $(".lt_topbar").toggleClass("hide_menu");
+    $(".lt_main").toggleClass("hide_menu");
+  });
+
+  $(".lt_topbar .icon_out").click(function () {
+    $("#logout_model").modal("show");
+  });
+
+  $("#logout_btn").click(function () {
+    $.ajax({
+      type: "get",
+      url: "/employee/employeeLogout",
+      success: function (info) {
+        console.log(info);
+        if (info.success === true) {
+          location.href = "login.html";
+        }
+      }
+    })
+  });
 });
