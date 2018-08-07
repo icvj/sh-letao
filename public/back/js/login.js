1e764b68de7242e52d6f612bf3d35f58f067a25a
jingtian <jingtian9573@163.com>
2018-08-07 20:09:17 +0800

    登录页完成，首页静态布局完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -1,6 +1,5 @@
 $(function () {
 
-
   $("#form").bootstrapValidator({
     //设置小图标
     feedbackIcons: {
@@ -20,6 +19,9 @@ $(function () {
                   min: 2,
                   max: 6,
                   message:"用户名长度必须是2-6位"
+              },
+              callback: {
+                message: "用户名不存在"
               }
           }
       },
@@ -32,6 +34,9 @@ $(function () {
               min: 6,
               max: 12,
               message: "密码必须是6-12位"
+            },
+            callback: {
+              message: "密码错误"
             }
           }
       }
@@ -41,5 +46,32 @@ $(function () {
   $("[type='reset']").click(function () {
     $("form").data("bootstrapValidator").resetForm();
   });
+
+  $("#form").on("success.form.bv", function (e) {
+    e.preventDefault();
+
+    $.ajax({
+      type: "post",
+      url: "/employee/employeeLogin",
+      data: $("#form").serialize(),
+      success: function (info) {
+        console.log(info);
+        //{success: true}
+        if (info.success) {
+          location.href = "index.html";
+        }
+
+        // {error: 1000, message: "用户名不存在! "}
+        if (info.error === 1000) {
+          $("#form").data("bootstrapValidator").updateStatus("username", "INVALID", "callback");
+        }
+        
+        // {error: 1001, message: "密码错误！"}
+        if (info.error === 1001) {
+          $("#form").data("bootstrapValidator").updateStatus("password", "INVALID", "callback");
+        }
+      }
+    })
+  })
 
 })
