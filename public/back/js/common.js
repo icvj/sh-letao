14666ad0ba6b54921879925139dc714d955c0c24
jingtian <jingtian9573@163.com>
2018-08-07 20:24:51 +0800

    新增进度条

---
+++ public/back/js/common.js
@@ -0,0 +1,15 @@
+$(function () {
+
+  
+  $(document).ajaxStart(function () {
+    NProgress.start();
+  });
+
+  $(document).ajaxStop(function () {
+    setTimeout(function () {
+      NProgress.done();
+    }, 500);
+  });
+
+
+});
